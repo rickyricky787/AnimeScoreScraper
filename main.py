763d773c35b0de02c73df1a8830f8cb9e9bf9aa8
rickyricky787 <rrodriguez2017@gmail.com>
2020-08-28 20:45:33 -0400

    Small fix on main function

--- main.py
+++ main.py
@@ -8,28 +8,29 @@ def TableOutput(website, name, score, votes, link):
     print("%-12s %-15.15s %-8s %-8s" % (website, name, score, votes), link)
         
 # Main function
-while (True):
-    anime_search = input("Enter name of anime: ")
-    print()
-    print("Scraping...")
-    print()
+if __name__ == "__main__":
+    while (True):
+        anime_search = input("Enter name of anime: ")
+        print()
+        print("Scraping...")
+        print()
 
-    data1 = MyAnimeList(anime_search)
-    data2 = AnimePlanet(anime_search)
-    data3 = AniList(anime_search)
+        data1 = MyAnimeList(anime_search)
+        data2 = AnimePlanet(anime_search)
+        data3 = AniList(anime_search)
 
-    TableOutput("Website", "Anime Name", "Score", "Votes", "Link")
-    TableOutput("MyAnimeList", data1.title, data1.conv_score, data1.votes, data1.link)
-    TableOutput("AnimePlanet", data2.title, data2.conv_score, data2.votes, data2.link)
-    TableOutput("Anilist", data3.title, data3.conv_score, data3.votes, data3.link)
+        TableOutput("Website", "Anime Name", "Score", "Votes", "Link")
+        TableOutput("MyAnimeList", data1.title, data1.conv_score, data1.votes, data1.link)
+        TableOutput("AnimePlanet", data2.title, data2.conv_score, data2.votes, data2.link)
+        TableOutput("Anilist", data3.title, data3.conv_score, data3.votes, data3.link)
 
-    print()
-    print("Scores were converted to a scale of 100.")
+        print()
+        print("Scores were converted to a scale of 100.")
 
-    print()
-    command = input("Enter 'Y' to perform another search. Enter anything else to quit: ")
-    print()
-    if command == 'Y' or command == 'y':
-        continue
-    else:
-        break
+        print()
+        command = input("Enter 'Y' to perform another search. Enter anything else to quit: ")
+        print()
+        if command == 'Y' or command == 'y':
+            continue
+        else:
+            break
